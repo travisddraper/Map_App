d7baf0abbeb63b2e422e91c9b3993ca69171e530
Travis Draper <tdanieldraper@gmail.com>
2021-03-26 19:15:03 -0400

    .

--- application.js
+++ application.js
@@ -31,33 +31,80 @@ var loadServer = function() {
         success: function(response, textStatus) {
 
             $('.icon').remove();
-            console.log('icons in server',response.tasks.length);
 
-
-            
-            //datapacket = [x,z,name]
             response.tasks.forEach(function(task) {
                 var dataPacket = task.content.split(' ');
                 if(Array.isArray(dataPacket)) {
-                    
                     var name = dataPacket[2];
-                    console.log(name);
+                    var color;
+                    var text;
 
 
+                    if(/red/.test(name)) {
 
+                        name = name.replace('red','');
+                        color = 'red';
 
+                        if(/X/.test(name)) {
 
+                            name = 'redX'
+                            text = 'X'
 
+                        }else if (/!/.test(name)) {
 
+                            name = 'redAH'
+                            text = "!"
+
+                        }else if(/\\?/.test(name)) {
+
+                            name = 'redQ'
+                            text = "?"
+
+                        }else if(/0/.test(name)) {
+
+                            text = name.replace('0','');
+                            name = 'newCharacter'
+
+                        }else {
+                            text = name;
+                        }
+                    } else if (/blue/.test(name)) {
+
+                        name = name.replace('blue','');
+                        color = 'blue';
+                        
+                        if(/X/.test(name)) {
+                           
+                            name = 'blueX'
+                            text = 'X'
+                            
+                        }else if (/!/.test(name)) {
+                            
+                            name = 'blueAH'
+                            text = "!"
+                            
+                        }else if(/\\?/.test(name)) {
+                            
+                            name = 'blueQ'
+                            text = "?"
+                            
+                        }else if(/0/.test(name)) {
+
+                            text = name.replace('0','');
+                            name = 'newCharacter'
+
+                        } else {
+                            text = name;
+                        }
+                    }
+
+                    
                     var x = dataPacket[0]
                     var y = dataPacket[1]
-                    
-                    $('#' + name).prepend($('<div style="left:' + x +'px; top:' + y + 'px" class="icon" data-id="' + task.id + '">'+ name + '</div>'))
-                    console.log('injected', task.id);
-                    
 
 
-                    //INJECT IT INTO THE DOM AT SPECIFIED X-Y POSITION WITH DATA ID AND CSS STYLING CLASSES;
+
+                    $('#' + name).prepend($('<div style="left:' + x +'px; top:' + y + 'px" class="icon ' + color +'" data-id="' + task.id + '">'+ text + '</div>'))
                 }
             })
         },
@@ -88,22 +135,29 @@ var addNewIconServer = function(datapacket) {
     });
 }
 
-/*
-var changeServerPosition = function(largerdatapacket) {
-    ajax requests {
+
+var changeServerPosition = function(datapacketz, id) {
+    $.ajax({
         type: 'PUT',
-        url: 'link with id',
+        url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '/mark_complete?api_key=28',
+        contentType: 'application/json',
+        dataType: 'json',
         data: JSON.stringify({
             task: {
-                content: largerdatapacket [x,y,name,id]
+                content: datapacketz
             }
         }),
         success: function(response, textStatus) {
-            loadServer();
+            console.log('datapacket from move:', datapacketz)
+            console.log('server response:', response);
         },
-    }
+        error: function(request, textStatus, errorMessage) {
+            console.log(errorMessage);
+        }
+    });
 }
 
+/*
 var loadServerImage = function(id) {
     ajax request {
         type: 'GET'
@@ -148,8 +202,9 @@ var deleteIconServer = function(id) {
     
 
     
-var toggleMove = function(elmnt, e) {
+var toggleMove = function(elmnt, e, iconText) {
     var pos1=0; var pos2=0; var pos3=0; var pos4=0;
+    var datapacket;
 
     var elementDrag = function(e) {
 
@@ -160,14 +215,28 @@ var toggleMove = function(elmnt, e) {
         pos3 = e.clientX;
         pos4 = e.clientY;
         elmnt.style.top = (elmnt.offsetTop - pos2) + 'px';
-  
+        var y = elmnt.style.top;
         elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px';
+        var x = elmnt.offsetLeft;
+       
+
+        //var datapacket = [x, y, iconText];
+        //console.log(datapacket);
+        
     }
 
     var closeDragElement = function() {
-
+        
         document.onmouseup = null;
         document.onmousemove = null;
+        var x = elmnt.style.left.replace('px', '');
+        var y = elmnt.style.top.replace('px','');
+
+        var datapacketz = [x, y, iconText].join(' ');
+        var id = $(elmnt).data('id');
+
+
+        changeServerPosition(datapacketz, id)
     }
 
     var dragMouseDown = function(e) {
@@ -194,24 +263,47 @@ var toggleMove = function(elmnt, e) {
 
 $(document).ready(function() {
 
+    
     loadServer();
-
+    
     $('.iconButton').on('click', function() {
         var iconText = $(this).text();
-        //buildAButton($(this), iconText);
+   
+        if(($(this).hasClass('red'))) {
+            iconText = iconText+"red"
+        }
+        if(($(this).hasClass('blue'))) {
+            iconText = iconText+"blue"
+        }
+
         var datapacket = [21, -49, iconText].join(' ');
-        console.log(datapacket);
+        console.log('data packet from new', datapacket);
         addNewIconServer(datapacket)
     })
 
     $('#addIconForm').on('submit', function(e) {
         e.preventDefault();
-        var iconText = $('#addIconInput').val();
+        var nameInput = $('#addIconInput').val();
+        var color = $("input[name=colorToggle]:checked").val()
+        var iconText = nameInput+color+0;
+        
+        datapacket = [21, -49, iconText].join(' ');
+        console.log(datapacket.split(' '));
+        addNewIconServer(datapacket);
     });
 
     $(document).on('mousedown', '.icon', function(e) {
+        var iconText = $(this).text()
+
+        if(($(this).hasClass('red'))) {
+            iconText = iconText+"red"
+        } else if(($(this).hasClass('blue'))) {
+            iconText = iconText+"blue"
+        }
+
         var elmnt = $(this)[0]
-        toggleMove(elmnt, e);
+        toggleMove(elmnt, e, iconText);
+        
     })
 
     $(document).on('contextmenu', '.icon', function(event) {
