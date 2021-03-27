3660d4115638611e9c84c9aa27492e8bc8c6bfd6
Travis Draper <tdanieldraper@gmail.com>
2021-03-27 16:08:43 -0400

    finalized rough draft

--- application.js
+++ application.js
@@ -1,24 +1,3 @@
-/*
-> Create CSS stylings for the ICON which is created from button press
-------js-------
-> Create a BUTTONPRESS function for each existing Icon that will produce a draggable div element with matching content (ideally above the button).
-
-> Create a function that will take the input of the textbox Icon (AS WELL AS COLOR RADIO BUTTON) and upon submitting runs the BUTTONPRESS function to create a draggable div element with submitted text as content in correct color
-
------server-----
-> Create a LOADSERVERIMAGE function that updates the img src="_____" with a GET ID## request 
-
-> Create a LOADSERVERPOSITIONS function that with a GET request (that filters for content value which is in array form)
-
-> Create a LOADSERVER function that LOADSSERVERPOSITIONS
-
-> Create a function that accepts an image url, PUTS it to the server (with specific ID##), and then runs LOADSERVER function
-
-PAGE LOADS (loadServer(), LoadServerImage(id)) --> BUTTON ICON IS PRESSED (addNewIconServer()) --> ICON IS DRAGGED (changeServerPosition(id)) --
---> 3 SECONDS GOES BY AND YOU DON'T INTERACT WITH WINDOW (loadServer());
-
-*/
-
 function isUpperCase(string) {
    return /^[A-Z]*$/.test(string);
 }
@@ -74,7 +53,7 @@ var loadServer = function() {
 
                         name = name.replace('blue','');
                         color = 'blue';
-                   
+        
                         if(/X/.test(name)) {
                            
                             name = 'blueX'
@@ -106,11 +85,11 @@ var loadServer = function() {
 
 
                     
-                    $('#' + name).prepend($('<div style="left:' + x +'px; top:' + y + 'px" class="icon ' + color +'" data-id="' + task.id + '">'+ text + '</div>'))
+                    $(".overlay").prepend($('<div style="left:' + x +'px; top:' + y + 'px" class="icon ' + color +'" data-id="' + task.id + '">'+ text + '</div>'))
                    
                 } else {
-                    console.log('HEYYY');
-                    $('#image').attr("src", task.content)
+                    
+                    $('#aspectRatioBox').css("background-image", "url(" + task.content + ")");
                 }
             })
         },
@@ -170,7 +149,7 @@ var loadServerImage = function(id) {
         url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks/' + id + '?api_key=29',
         dataType: 'json',
         success: function(response, textStatus) {
-            $('#image').attr("src", response.task.content)
+            $('.aspectRatioBox').attr("background-image", response.task.content)
         },
         error: function(request, textStatus, errorMessage) {
             console.log(errorMessage);
@@ -236,7 +215,7 @@ var toggleMove = function(elmnt, e, iconText) {
         elmnt.style.left = (elmnt.offsetLeft - pos1) + 'px';
         var x = elmnt.offsetLeft;
        
-
+       
         //var datapacket = [x, y, iconText];
         //console.log(datapacket);
         
@@ -251,6 +230,7 @@ var toggleMove = function(elmnt, e, iconText) {
 
         var datapacketz = [x, y, iconText].join(' ');
         var id = $(elmnt).data('id');
+        console.log(datapacketz);
 
         changeServerPosition(datapacketz, id)
     }
@@ -313,21 +293,21 @@ $(document).ready(function() {
 
     
     loadServer();
+  
 
     var interval;
 
-    $(window).on('keydown keyup click mousemove change', function(event) {
-        interval = intervalReset(interval);
-        refreshThis();
-    })
+    //$(window).on('keydown keyup click mousemove change', function(event) {
+        //interval = intervalReset(interval);
+        //refreshThis();
+    //})
 
 
     $('#imageForm').on('submit', function(e) {
         e.preventDefault();
         var imageURL = $('#imageInput').val()
-
-
         replaceServerImage(imageURL, 1155)
+        $('#imageInput').val('')
     })
 
     
@@ -341,7 +321,7 @@ $(document).ready(function() {
             iconText = iconText+"blue"
         }
 
-        var datapacket = [21, -49, iconText].join(' ');
+        var datapacket = [469, 703, iconText].join(' ');
  
         addNewIconServer(datapacket)
     })
@@ -352,7 +332,7 @@ $(document).ready(function() {
         var color = $("input[name=colorToggle]:checked").val()
         var iconText = nameInput+color+0;
         
-        datapacket = [21, -49, iconText].join(' ');
+        datapacket = [469, 703, iconText].join(' ');
 
         addNewIconServer(datapacket);
     });
